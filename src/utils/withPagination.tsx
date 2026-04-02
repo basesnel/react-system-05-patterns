dbc28ab6b1154c3c5f6da423223ad82b88b1fa44
basesnel <basesnel@gmail.com>
2026-04-02 15:25:40 +0300

    HOC-component is added.

---
+++ src/utils/withPagination.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import type { ComponentType } from "react";
+
+function withPagination<P>(WrappedComponent: ComponentType<P>) {
+  return (props: P) => {
+    const [page, setPage] = useState(1);
+
+    const nextPage = () => setPage((prev) => prev + 1);
+    const prevPage = () => setPage((prev) => Math.max(prev + 1, 1));
+
+    return (
+      <div>
+        <WrappedComponent {...props} page={page} />
+        <div>
+          <button onClick={prevPage}>Prev</button>
+          <button onClick={nextPage}>Next</button>
+        </div>
+      </div>
+    );
+  };
+}
+
+export { withPagination };

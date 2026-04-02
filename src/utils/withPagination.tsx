bbecb88dc69110c719346b9023868ff12ffc5088
basesnel <basesnel@gmail.com>
2026-04-02 16:48:33 +0300

    withPagination is corrected.

--- src/utils/withPagination.tsx
+++ src/utils/withPagination.tsx
@@ -6,7 +6,7 @@ const withPagination = <P,>(WrappedComponent: ComponentType<P>) => {
     const [page, setPage] = useState(1);
 
     const nextPage = () => setPage((prev) => prev + 1);
-    const prevPage = () => setPage((prev) => Math.max(prev + 1, 1));
+    const prevPage = () => setPage((prev) => Math.max(prev - 1, 1));
 
     return (
       <div>

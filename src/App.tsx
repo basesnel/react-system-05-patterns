af247644846fd227af8e02fa729d8ea72e5429ae
basesnel <basesnel@gmail.com>
2026-04-03 21:07:52 +0300

    LoginForm is added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 // import { UsersV02 } from "./components/UsersV02/UsersV02";
 // import { UsersV01 } from "./components/UsersV01/UsersV01";
-import { UsersV03 } from "./components/UsersV03/UsersV03";
-import { UsersWithPosts } from "./components/UsersWithPosts/UsersWithPosts";
+// import { UsersV03 } from "./components/UsersV03/UsersV03";
+// import { UsersWithPosts } from "./components/UsersWithPosts/UsersWithPosts";
+
+import { LoginForm } from "./components/LoginForm/LoginForm";
 
 function App() {
   return (
@@ -9,8 +11,9 @@ function App() {
       <h1>App</h1>
       {/* <UsersV01 /> */}
       {/* <UsersV02 /> */}
-      <UsersV03 />
-      <UsersWithPosts />
+      {/* <UsersV03 /> */}
+      {/* <UsersWithPosts /> */}
+      <LoginForm />
     </>
   );
 }
---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,33 @@
+import { useLoginForm } from "../../utils/useLoginForm";
+
+const LoginForm = () => {
+  const { values, errors, handleChange, handleSubmit } = useLoginForm();
+
+  return (
+    <form
+      onSubmit={(e) => handleSubmit(e, (data) => console.log("Login:", data))}
+    >
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      {errors.email && <span>{errors.email}</span>}
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      {errors.password && <span>{errors.password}</span>}
+
+      <button>Log In</button>
+    </form>
+  );
+};
+
+export { LoginForm };

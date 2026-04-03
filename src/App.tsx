b9d4cfa3f0036306c7798a75e535a2c049f501fc
basesnel <basesnel@gmail.com>
2026-04-03 11:34:50 +0300

    UserCard & PostCard are applied.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { UsersV02 } from "./components/UsersV02/UsersV02";
 // import { UsersV01 } from "./components/UsersV01/UsersV01";
+import { UsersV03 } from "./components/UsersV03/UsersV03";
 import { UsersWithPosts } from "./components/UsersWithPosts/UsersWithPosts";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <h1>App</h1>
       {/* <UsersV01 /> */}
       {/* <UsersV02 /> */}
+      <UsersV03 />
       <UsersWithPosts />
     </>
   );
---
+++ src/components/UserPostsV03/UserPostsV03.tsx
@@ -0,0 +1,36 @@
+import { useFetch } from "../../utils/useFetch";
+import { PostCard } from "../PostCard/PostCard";
+
+interface Props {
+  userId: number;
+}
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+const UserPostsV03 = ({ userId }: Props) => {
+  const { data, loading, error } = useFetch<Post[]>(
+    `https:/jsonplaceholder.typicode.com/posts?userId=${userId}`,
+  );
+
+  if (loading) return <div>Posts fetching...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      <h2>User {userId}'s posts</h2>
+      <ul>
+        {data?.map((post) => (
+          <li key={post.id}>
+            <PostCard post={post} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export { UserPostsV03 };

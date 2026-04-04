9fffb9b35e5287524be1ba65494ed7b1da2765ac
basesnel <basesnel@gmail.com>
2026-04-04 11:08:49 +0300

    TabWithFilters is added.

---
+++ src/components/UsersTabWithFilters/UsersTabWithFilters.tsx
@@ -0,0 +1,30 @@
+import { TableWithFilters } from "../TabWithFilters/TabWithFilters";
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+interface Props {
+  users: User[];
+}
+
+function UsersTableWithFilters({ users }: Props) {
+  return (
+    <TableWithFilters<User>
+      data={users}
+      columns={["Name", "Email"]}
+      renderRow={(user) => (
+        <tr key={user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+        </tr>
+      )}
+      sortStrategy={userSortStrategy}
+      filterStrategy={userFilterStrategy}
+    />
+  );
+}
+
+export { UsersTableWithFilters };

0e0b45d77d541a4fb627df95a7cf43b4aaf6335e
basesnel <basesnel@gmail.com>
2026-04-04 15:49:53 +0300

    Another example is added.

---
+++ src/utils/useFetchV2.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+type UseFetchResult<T> = {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+};
+
+const useFetchV2 = <T>(url: string): UseFetchResult<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        return response.json();
+      })
+      .then(setData)
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export { useFetchV2 };

d9925047b1041ca66b38292f237c0b68d9625735
basesnel <basesnel@gmail.com>
2026-04-08 21:39:07 +0300

    Component Filters is added.

---
+++ src/components/02_SOLID/Filter/Filter.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  phone: string;
+  webSite: string;
+};
+
+interface Props {
+  users: User[];
+}
+
+const Filter = ({ users }: Props) => {
+  const [filter, setFilter] = useState("all");
+
+  const filteredUsers = users.filter((user) => {
+    if (filter === "all") return true;
+    if (filter === "hasPhone") return user.phone;
+    if (filter === "hasWebsite") return user.webSite;
+  });
+
+  return (
+    <div>
+      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+        <option value="all">All Users</option>
+        <option value="hasPhone">Users with Phone</option>
+        <option value="hasWebsite">Users with Website</option>
+      </select>
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export { Filter };

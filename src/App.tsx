c0dcf5ec0fbc3cfaea52ca0e490a35e21425ffbb
basesnel <basesnel@gmail.com>
2026-04-08 21:20:29 +0300

    Component Users is added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import { Box } from "./components/01_OOP/Box/Box";
-import { CommentForm } from "./components/01_OOP/CommentForm/CommentForm";
-import { HoverBox } from "./components/01_OOP/HoverBox/HoverBox";
-import { LoginForm } from "./components/01_OOP/LoginForm/LoginForm";
-import { UsersV04 } from "./components/01_OOP/UsersV04/UsersV04";
-import { UsersV05 } from "./components/01_OOP/UsersV05/UsersV05";
-import { useFetchUserId } from "./utils/fetchData";
+import { Users } from "./components/02_SOLID/Users/Users";
 
 function App() {
-  useFetchUserId();
-
   return (
     <>
       <h1>App</h1>
-      <LoginForm />
-      <CommentForm />
-      <UsersV04 />
-      <Box as="section">It's a section</Box>
-      <Box as="button" onClick={() => alert("Clicked!")}>
-        Button
-      </Box>
-      <Box as="a" href="https://react.dev">
-        Link
-      </Box>
-      <HoverBox
-        render={(hovered) => (hovered ? "Got hovered me!" : "Hover me!")}
-      />
-      <HoverBox
-        render={(hovered) => (
-          <button
-            style={{ background: hovered ? "blue" : "gray", color: "white" }}
-          >
-            {hovered ? "Got hovered!" : "Click me!"}
-          </button>
-        )}
-      />
-      <UsersV05 />
+      <Users />
     </>
   );
 }
---
+++ src/components/02_SOLID/Users/Users.tsx
@@ -0,0 +1,28 @@
+import { useFetch } from "../../../utils/useFetch";
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const Users = () => {
+  const { data, loading, error } = useFetch<User[]>(
+    "https:/jsonplaceholder.typicode.com/users",
+  );
+
+  if (loading) return <div>Users fetching...</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {data?.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export { Users };

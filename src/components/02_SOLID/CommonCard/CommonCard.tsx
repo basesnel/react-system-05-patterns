166075b5f3ac10eb7198a7b9f46f902962a47176
basesnel <basesnel@gmail.com>
2026-04-09 16:49:34 +0300

    Ctyled a Common Card.

--- src/components/02_SOLID/CommonCard/CommonCard.tsx
+++ src/components/02_SOLID/CommonCard/CommonCard.tsx
@@ -3,9 +3,11 @@ interface CardProps {
   renderBody: () => React.ReactNode;
 }
 
+import styles from "./styles.module.css";
+
 // LSP - Liskov Substitution Principle (correct)
 const CommonCard: React.FC<CardProps> = ({ renderHeader, renderBody }) => (
-  <div className="card">
+  <div className={styles.card}>
     <h3>{renderHeader()}</h3>
     <p>{renderBody()}</p>
   </div>

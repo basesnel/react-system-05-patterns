ee40b846c0a9d2ecc94cc5e06a64106892222ea4
basesnel <basesnel@gmail.com>
2026-04-09 08:45:05 +0300

    Component CommonFilter is added.

---
+++ src/components/02_SOLID/ComonFilter/ComonFilter.tsx
@@ -0,0 +1,33 @@
+import { useState, type ReactNode } from "react";
+
+interface FilterProps<T> {
+  data: T[];
+  filters: {
+    [key: string]: (item: T) => boolean;
+  };
+  renderItem: (item: T) => ReactNode;
+}
+
+const CommonFilter = <T,>({ data, filters, renderItem }: FilterProps<T>) => {
+  const [activeFilter, setActiveFilter] = useState<string>("all");
+
+  const filteredData = data.filter(filters[activeFilter]);
+
+  return (
+    <div>
+      <select
+        value={activeFilter}
+        onChange={(e) => setActiveFilter(e.target.value)}
+      >
+        {Object.keys(filters).map((key) => (
+          <option key={key} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+      <ul>{filteredData.map(renderItem)}</ul>
+    </div>
+  );
+};
+
+export { CommonFilter };

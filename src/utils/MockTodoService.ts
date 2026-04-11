436485127f089001ec087b36dfd881baa1b2c5dc
basesnel <basesnel@gmail.com>
2026-04-11 14:44:55 +0300

    Added a service MockTodoService.

---
+++ src/utils/MockTodoService.ts
@@ -0,0 +1,37 @@
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+import type { TodoService } from "./ApiTodoService";
+
+export class MockTodoService implements TodoService {
+  private todos: Todo[];
+
+  constructor() {
+    this.todos = [
+      { id: 1, title: "Test Todo 1", completed: false },
+      { id: 2, title: "Test Todo 2", completed: false },
+    ];
+  }
+
+  async getTodos(): Promise<Todo[]> {
+    return this.todos;
+  }
+
+  async addTodo(todo: Partial<Todo>): Promise<Todo> {
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: todo.title || "Untitled",
+      completed: todo.completed ?? false,
+    };
+    this.todos.push(newTodo);
+    return newTodo;
+  }
+
+  async deleteTodo(id: number): Promise<number> {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+    return id;
+  }
+}

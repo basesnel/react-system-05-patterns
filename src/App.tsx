61c121fa0e50c83eb0f36c939a25a2d9be2ee951
basesnel <basesnel@gmail.com>
2026-04-16 22:23:58 +0300

    Added ListRenderer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-// import { Posts } from "./components/02_SOLID/Posts/Posts";
-// import { Users } from "./components/02_SOLID/Users/Users";
-import { TodoListV01 } from "./components/02_SOLID/TodoListV01/TodoListV01";
-import { TodoListV02 } from "./components/02_SOLID/TodoListV02/TodoListV02";
-// import { ApiTodoService } from "./utils/ApiTodoService";
-import { MockTodoService } from "./utils/MockTodoService";
-
-// const todoService = new ApiTodoService();
-const todoService = new MockTodoService();
+import { ListRenderer } from "./components/03_PATTERNS/ListRenderer/ListRenderer";
 
 function App() {
+  const items = ["Apple", "Banana", "Cherry"];
+
   return (
     <>
       <h1>App</h1>
-      <TodoListV01 />
-      <TodoListV02 todoService={todoService} />
-      {/* <Users />
-      <Posts /> */}
+      <ListRenderer items={items} render={(item) => <p key={item}>{item}</p>} />
     </>
   );
 }
---
+++ src/components/03_PATTERNS/ListRenderer/ListRenderer.tsx
@@ -0,0 +1,11 @@
+const ListRenderer = ({ items, render }) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+};
+
+export { ListRenderer };
